e4283bd195d97a46ca377e0d92104f083c9aa9eb
dmesic <dario.mesic@fer.hr>
2023-09-13 15:51:58 +0200

    added chatting with bot

--- src/services/data.services.js
+++ src/services/data.services.js
@@ -80,5 +80,184 @@ class DataService {
         return response.json();
     })
   }
+
+  sendQuestions(){
+    return fetch('/api/sendQuestions', {
+      method : "GET",
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(response.error)
+        }
+        return response.json();
+    })
+  }
+
+  sendMessage(question){
+    return fetch('/api/chatbotSentMessage', {
+      method : "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({question: question})
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(response.error)
+        }
+        return response.json();
+    })
+  }
+
+  userResponse(conditions, intent_id){
+    return fetch('/api/chatbotUserResponse', {
+      method : "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({conditions: conditions, intent_id: intent_id})
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(response.error)
+        }
+        return response.json();
+    })
+  }
 }
 export default new DataService();
+
+/*
+class DataService {
+  getIntents() {
+    return fetch('getIntents', {
+      method: "GET",
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.error);
+      }
+      return response.json();
+    });
+  }
+  
+  getQuestionsForIntent(intent_id){
+    return fetch('/getQuestionsForIntent?intent_id=' + intent_id, {
+      method : "GET",
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(response.error)
+        }
+        return response.json();
+    })
+  }
+
+  postQuestion(question, intent_id){
+    return fetch('/postQuestion', {
+      method : "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({question: question, intent_id: intent_id})
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(response.error)
+        }
+        return response.json();
+    })
+  }
+
+  deleteQuestion(question_id){
+    return fetch('/deleteQuestion?question_id=' + question_id, {
+      method : "DELETE",
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(response.error)
+        }
+        return response.json();
+    })
+  }
+
+  updateQuestion(new_question, question_id){
+    return fetch('/updateQuestion', {
+      method : "PUT",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({new_question: new_question, question_id : question_id})
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(response.error)
+        }
+        return response.json();
+    })
+  }
+
+  getRulesForIntent(intent_id){
+    return fetch('/getRulesForIntent?intent_id=' + intent_id, {
+      method : "GET",
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(response.error)
+        }
+        return response.json();
+    })
+  }
+
+  sendQuestions(){
+    return fetch('/sendQuestions', {
+      method : "GET",
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(response.error)
+        }
+        return response.json();
+    })
+  }
+
+  sendMessage(question){
+    return fetch('/chatbotSentMessage', {
+      method : "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({question: question})
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(response.error)
+        }
+        return response.json();
+    })
+  }
+
+  userResponse(conditions, intent_id){
+    return fetch('/chatbotUserResponse', {
+      method : "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({conditions: conditions, intent_id: intent_id})
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(response.error)
+        }
+        return response.json();
+    })
+  }
+}
+export default new DataService();
+*/
