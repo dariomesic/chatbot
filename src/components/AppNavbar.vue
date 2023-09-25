91ff9d878ffae42ed5f1294838c38912ebdb83a4
dmesic <dario.mesic@fer.hr>
2023-09-25 21:47:27 +0200

    added logo

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,7 +1,13 @@
 <template>
     <nav class="navbar">
       <div style="padding-left:1%;height:35px">
-        <img src="https://mpu.gov.hr/img/mp_logo@2x.png" style="height:inherit;background:white">
+        <a href="https://nesolventnost.pravosudje.hr/">
+          <img src="../assets/grb.png" height="35" alt="Republika Hrvatska">
+          <h2>
+            <p>REPUBLIKA HRVATSKA</p>
+            <p style="margin-top:3px">Ministarstvo pravosuđa i uprave</p>
+          </h2>
+        </a>
       </div>
     </nav>
 </template>
@@ -12,5 +18,18 @@
   color: #fff;
   display: flex;
   align-items: center;
+}
+
+.navbar a{
+  display: flex;
+  align-items: center;
+  color: #fcfcfd;
+  text-decoration: none;
+}
+
+.navbar h2{
+  margin-left: 1rem;
+  font-weight: bolder;
+  font-size: .635rem;
 }
 </style>
