aaeefc92e75c1d7d09b6606315ea9a9862791831
dmesic <dario.mesic@fer.hr>
2023-10-13 00:18:38 +0200

    small bug fix on event

--- src/components/textarea/TextEditor.vue
+++ src/components/textarea/TextEditor.vue
@@ -223,7 +223,7 @@ export default {
                 aria-label="Edit"
                 aria-hidden="true"
                 role="img"
-                class="bx--btn__icon"
+                class="edit"
               >
                 <path
                   d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z"
@@ -245,7 +245,7 @@ export default {
                 height="18"
                 viewBox="0 0 32 32"
                 role="img"
-                class="bx--btn__icon"
+                class="delete"
               >
                 <path d="M12 12H14V24H12zM18 12H20V24H18z"></path>
                 <path
@@ -510,10 +510,9 @@ export default {
     this.isActive();
     const editorWrapper = document.querySelector(".textarea");
     editorWrapper.addEventListener("click", (event) => {
-      const clickedButton = event.target;
-      if (clickedButton.ariaLabel === "Edit") {
+      if (event.target.classList.contains('edit')) {
         this.editTimer(event);
-      } else if (clickedButton.ariaLabel === "Delete") {
+      } else if (event.target.classList.contains('delete')) {
         this.deleteTimer(event);
       }
     });
