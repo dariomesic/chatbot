ab436a098a0acca3971242af99cb228f6219a322
BrunoK2410 <brunokatic2410@gmail.com>
2023-11-20 11:52:43 +0100

    Added new route

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,38 @@
 import { createRouter, createWebHistory } from "vue-router";
 const HomePage = () => import("../views/HomePage.vue");
 const IntentList = () => import("../views/IntentList.vue");
-const IntentRules  = () => import("../views/IntentRules.vue");
+const IntentRules = () => import("../views/IntentRules.vue");
+const IntentsAndConversations = () =>
+  import("../views/IntentsAndConversations.vue");
 
 const routes = [
-    {
-        path: "/",
-        component: HomePage,
-    },
-    {
-        name: 'Intents',
-        path: "/intents",
-        component: IntentList,
-    },
-    { 
-      path: '/intents/intent', 
-      name: 'IntentRules', 
-      component: IntentRules,
-    },
-    {
-      path: "/:catchAll(.*)",
-      name: "NotFound",
-    }
+  {
+    path: "/",
+    component: HomePage,
+  },
+  {
+    name: "Intents",
+    path: "/intents",
+    component: IntentList,
+  },
+  {
+    path: "/intents/intent",
+    name: "IntentRules",
+    component: IntentRules,
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: "NotFound",
+  },
+  {
+    path: "/intentsandconversations",
+    name: "IntentsAndConversations",
+    component: IntentsAndConversations,
+  },
 ];
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
 });
 
 export default router;
