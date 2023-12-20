deb5a9b8f346d3511b47aca89d7fdd0177de9d56
Dario Mesić <dario@aitech.hr>
2023-12-20 15:02:38 +0100

    removed status column

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -37,7 +37,6 @@
               </span>
             </div>
           </th>
-          <th>Status</th>
         </tr>
       </thead>
       <tr v-for="chatbot in chatbots" :key="chatbot.id">
@@ -93,37 +92,6 @@
         </td>
         <td>
           <span>{{ chatbot.intents_count }}</span>
-        </td>
-        <td>
-          <span>
-            <button
-              style="cursor: default"
-              type="button"
-              aria-describedby="icon-tooltip-41"
-            >
-              <svg
-                style="fill: #42be65"
-                focusable="false"
-                preserveAspectRatio="xMidYMid meet"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                aria-hidden="true"
-                class="CollectionsDataTable__status-icon-no-error"
-              >
-                <path
-                  d="M8,1C4.1,1,1,4.1,1,8c0,3.9,3.1,7,7,7s7-3.1,7-7C15,4.1,11.9,1,8,1z M7,11L4.3,8.3l0.9-0.8L7,9.3l4-3.9l0.9,0.8L7,11z"
-                ></path>
-                <path
-                  d="M7,11L4.3,8.3l0.9-0.8L7,9.3l4-3.9l0.9,0.8L7,11z"
-                  data-icon-path="inner-path"
-                  opacity="0"
-                ></path>
-              </svg>
-            </button>
-          </span>
         </td>
       </tr>
     </table>
